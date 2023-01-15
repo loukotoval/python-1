cc34e4edd14a24ca34689b3ef8d772fc099f6838
Lucie Loukotova <lucie.loukotova@ujep.cz>
2023-01-15 18:03:24 +0100

    Velká písmena v iniciálách

--- DU_1_volitelny.py
+++ DU_1_volitelny.py
@@ -9,15 +9,17 @@ print(jmeno_a_prijmeni.upper())
 print(jmeno_a_prijmeni.lower())
 
 rozdelene = jmeno_a_prijmeni.split(" ")
-jmeno = jmeno_a_prijmeni.split(" ")[0]
-prijmeni = jmeno_a_prijmeni.split(" ")[1]
 
 # Pouze iniciály
-print(f"{rozdelene[0][0]}. {rozdelene[1][0]}.")
-print(f"{jmeno[0]}. {prijmeni[0]}.")
+print(f"{rozdelene.upper()[0][0]}. {rozdelene.upper()[1][0]}.")
 
 # U křestních jmen delších než 5 znaků zkrátit na iniciálu
 if len(rozdelene[0]) > 5:
     print(f"{rozdelene[0][0]}. {rozdelene[1]}")
 else:
     print(jmeno_a_prijmeni)
+
+
+#jmeno = jmeno_a_prijmeni.split(" ")[0]
+#prijmeni = jmeno_a_prijmeni.split(" ")[1]
+#print(f"{jmeno.upper()[0]}. {prijmeni.upper()[0]}.")
