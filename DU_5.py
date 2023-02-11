4ff3f37e6e963c0e43ec53996172e90671b262c8
Lucie Loukotova <lucie.loukotova@ujep.cz>
2023-02-11 17:48:34 +0100

    Řešení DÚ

---
+++ DU_5.py
@@ -0,0 +1,28 @@
+# Mějme zadaný následující seznam naměřených teplot. Seznam obsahuje teploty naměřené pro každý den v týdnu ve čtyřech časech - ráno, v poledne, večer a v noci.
+
+teploty = [
+    [2.1, 5.2, 6.1, -0.1],
+    [2.2, 4.8, 5.6, -1.0],
+    [3.3, 6.5, 5.9, 1.2],
+    [2.9, 5.6, 6.0, 0.0],
+    [2.0, 4.6, 5.5, -1.2],
+    [1.0, 3.2, 2.1, -2.0],
+    [0.4, 2.7, 1.3, -2.8]
+]
+
+# Vytvořte seznam průměrných teplot pro každý den.
+import statistics
+prumery = [round(statistics.mean(den),1) for den in teploty]
+print(prumery)
+
+# Vytvořte seznam ranních teplot.
+rano = [den[0] for den in teploty]
+print(rano)
+
+# Vytvořte seznam nočních teplot.
+noc = [den[3] for den in teploty]
+print(noc)
+
+# Vytvořte seznam dvouprvkových seznamů obsahujících pouze polední a noční teplotu.
+poledne_noc = [[den[1], den[3]] for den in teploty]
+print(poledne_noc)
