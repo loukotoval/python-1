9f028aebef41e4b2d8b7802f31879c80cb4aaf6a
Lucie Loukotova <lucie.loukotova@ujep.cz>
2023-02-12 19:28:04 +0100

    Zadání úkolu

---
+++ DU_4.py
@@ -0,0 +1,10 @@
+# Uvažuj, že píšeš jednoduchou aplikaci pro zasílání SMS zpráv. Napiš program, který provede následující činnosti:
+## Zeptá se uživatele na číslo, kam chce zprávu zaslat a ověří, že číslo má správný formát.
+## Zeptá se uživatele na zprávu, kterou chce zaslat. Následně vypíše, kolik zpráva bude stát.
+
+# Tvůj program bude obsahovat dvě funkce:
+## První funkce ověří telefonní číslo. Uvažuj, že telefonní číslo může být devítimístné nebo třináctimístné (pokud je na začátku předvolba "+420"). Funkce ověří, jestli je číslo platné. Pokud je platné, vrátí hodnotu True, jinak vrátí hodnotu False.
+## Druhá funkce spočte cenu zprávy. Uživatel platí 3 Kč za každých započatých 180 znaků.
+
+# Tvůj program nejprve ověří pomocí první funkce správnost telefonního čísla. Pokud není platné, vypíše chybu, v opačném případě se zeptá na text zprávy a pomocí druhé funkce určí její cenu, kterou vypíše uživateli.
+
